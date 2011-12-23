f173e82b450d28418add369204b28b2cc23b7a16
rsjtdrjgfuzkfg <rsjtdrjgfuzkfg@abwesend.de>
2011-12-23 13:20:29 +0100

    Added better error handling for XUL parsing. Last upload failed... wrong version of the file was uploaded; sorry for the terrible diff.

--- application/libraries/XPI_parser.php
+++ application/libraries/XPI_parser.php
@@ -1,65 +1,133 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class XPI_parser
+class Xpi_parser
 {
   /**
-	 * Get raw metadata from XPI file. Data is ONLY EXTRACTED.
-	 * Usually there is no need to access this directly, except you need
-	 * to access custom tags for special features within your application.
-	 *
-	 * @return array of arrays for each metadata, which contain the metadatas
-	 * found.
-	 **/
+   * Recursive fill raw metadata array with data from reader, parsing using
+   * htmlentities.
+   * 
+   * @return void
+   */
+  private function fillMeta(&$reader, &$meta){
+    while ($reader->read()) {
+      if ($reader->nodeType == XMLREADER::ELEMENT) {
+        if ($reader->namespaceURI == 'http://www.mozilla.org/2004/em-rdf#'){
+          $name = explode(':', $reader->name);
+          array_shift($name);
+          $nodename = implode(':', $name);
+        } else {
+          $nodename = "::$reader->name";
+        }
+        $this->fillMeta($reader, $meta[$nodename]);
+      }
+      else if ($reader->nodeType == XMLREADER::TEXT){
+        $meta[] = htmlentities($reader->value, ENT_QUOTES, 'UTF-8', false);
+      }
+      else if ($reader->nodeType == XMLREADER::END_ELEMENT){
+        return;
+      }
+    }
+  }
+  
+  /**
+   * Seek reader to metadata node
+   *
+   * @return void
+   */
+  private function gotoMeta(&$reader){
+    while ($reader->read()) {
+      if ($reader->nodeType == XMLREADER::ELEMENT) {
+        if (($reader->name == 'Description') && ($reader->getAttribute('about') == 'urn:mozilla:install-manifest'))
+          return;
+      }
+    }
+  }
+
+  /**
+   * Get raw metadata from XPI file. Data is ONLY EXTRACTED.
+   * Usually there is no need to access this directly, except you need
+   * to access custom tags for special features within your application.
+   * All Data is parsed usign htmlentities.
+   *
+   * @return array of arrays for each metadata, which contain the metadatas
+   * found, or null if the file is invalid.
+   **/
   public function getRawMetadata($filename){
     $reader = new XMLReader();
-    $reader->open('zip://' . $filename . '#install.rdf');
+    if (!@$reader->open('zip://' . $filename . '#install.rdf'))
+      return null;
     
-    function fillMeta(&$reader, &$meta){
-      while ($reader->read()) {
-        if ($reader->nodeType == XMLREADER::ELEMENT) {
-          if ($reader->namespaceURI == 'http://www.mozilla.org/2004/em-rdf#'){
-            $name = explode(':', $reader->name);
-            array_shift($name);
-            $nodename = implode(':', $name);
-          } else {
-            $nodename = "::$reader->name";
-          }
-          fillMeta($reader, $meta[$nodename]);
-        }
-        else if ($reader->nodeType == XMLREADER::TEXT){
-          $meta[] = htmlentities($reader->value, ENT_QUOTES, 'UTF-8', false);
-        }
-        else if ($reader->nodeType == XMLREADER::END_ELEMENT){
-          return;
-        }
-      }
-    }
-    
-    function gotoMeta(&$reader){
-      while ($reader->read()) {
-        if ($reader->nodeType == XMLREADER::ELEMENT) {
-          if (($reader->name == 'Description') && ($reader->getAttribute('about') == 'urn:mozilla:install-manifest'))
-            return;
-        }
-      }
-    }
-    
-    gotoMeta($reader);
-    fillMeta($reader, $meta);
+    $this->gotoMeta($reader);
+    $this->fillMeta($reader, $meta);
     
     return $meta;
   }
-	
-	/**
-	 * Get metadata from XPI file including localized descriptions,etc. Only
-	 * known, Mozilla-rdf Metadata will be available in the returned array.
-	 *
-	 * @return array of arrays or values for each metadata, valued for
-	 * unlocalizeable fields and arrays with locales for localizeable ones.
-	 * Locale 'default' is used for default fields.
-	 */
-	function getFullMetadata($filename){
-	  $result = [];
-		
-	}
+  
+  /**
+   * Get metadata from XPI file including localized descriptions,etc. Only
+   * known, Mozilla-rdf Metadata will be available in the returned array.
+   *
+   * @return array of arrays or values for each metadata, valued for
+   * unlocalizeable fields and arrays with locales for localizeable ones.
+   * Locale 'default' is used for default fields. If the file is invalid,
+   * null will get returned.
+   */
+  public function getMetadata($filename){
+    $result = array();
+    $raw = $this->getRawMetadata($filename);
+    if ($raw === null)
+      return null;
+    
+    $result['id'] = $raw['id'][0];
+    $result['type'] = $raw['type'][0];
+    $result['version'] = $raw['version'][0];
+    if (array_key_exists('iconURL', $raw))
+      $result['iconURL'] = $raw['iconURL'][0]; // see getIconURL
+    
+    $result['targetApplication'] = array();
+    $tAppCount = count($raw['targetApplication']['::Description']['id']);
+    for ($i = 0; $i < $tAppCount; $i++){
+      $tgApp = array();
+      $tgApp['id'] = $raw['targetApplication']['::Description']['id'][$i];
+      $tgApp['minVersion'] =
+          $raw['targetApplication']['::Description']['minVersion'][$i];
+      $tgApp['maxVersion'] =
+          $raw['targetApplication']['::Description']['maxVersion'][$i];
+      $result['targetApplication'][] = $tgApp;
+    }
+    
+    $localizeableTag = array( // false: only one value per locale
+        'name' => false,
+        'description' => false,
+        'creator' => true,
+        'contributor' => true,
+        'translator'=> true
+      );
+    foreach ($localizeableTag as $crt => $multiple){
+      if (!array_key_exists($crt, $raw)) continue;
+      $result[$crt] = array();
+      $result[$crt]['default'] = $multiple ? $raw[$crt] : $raw[$crt][0];
+      // We don't support localized fields for fields with multiple values yet.
+      if ((!$multiple) && array_key_exists('localized', $raw)){
+        $localeCount = count($raw['localized']['::Description']['locale']);
+        for ($i = 0; $i < $localeCount; $i++){
+          $result[$crt][$raw['localized']['::Description']['locale'][$i]] = $raw['localized']['::Description'][$crt][$i];
+        }
+      }
+    }
+    return $result;
+  }
+  
+  /**
+   * Get an icon by a chrome URL. This will parse the chrome.manifest and
+   * return an URL using the zip protocol to access the image file
+   * represented by the chrome url given. If the chrome url can't get
+   * resolved within the add-on, null will be returned.
+   *
+   * @return zip url pointing to the image or null
+   */
+  public function getIconURL($filename, $url){
+    // not implemented yet
+    return null;
+  }
 }
